1334e45a9674ee2cd8f2f27207a5db2bd115d1d4
Akshay Jain <30288355+wr3nch0x1@users.noreply.github.com>
2022-02-17 12:54:39 +0530

    added .json ext in output

--- core/output.go
+++ core/output.go
@@ -12,7 +12,7 @@ type Output struct {
 }
 
 func NewOutput(folder, filename string) *Output {
-	outFile := filepath.Join(folder, filename)
+	outFile := filepath.Join(folder, filename, ".json")
 	f, err := os.OpenFile(outFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, os.ModePerm)
 	if err != nil {
 		Logger.Errorf("Failed to open file to write Output: %s", err)
